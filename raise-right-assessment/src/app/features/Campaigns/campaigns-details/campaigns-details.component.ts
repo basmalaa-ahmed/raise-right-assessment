2a8695c29f81ca32841e34d64af2a77551760b43
basmalaa-ahmed <devbasmala.ahmed@gmail.com>
2025-08-20 07:15:22 +0300

    last

--- raise-right-assessment/src/app/features/Campaigns/campaigns-details/campaigns-details.component.ts
+++ raise-right-assessment/src/app/features/Campaigns/campaigns-details/campaigns-details.component.ts
@@ -4,57 +4,57 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Apollo, gql } from 'apollo-angular';
 import { CampaignService } from '../../../core/services/campaign.service';
-import { NotificationComponent } from "../../../shared/components/notification/notification.component";
+import { NotificationComponent } from '../../../shared/components/notification/notification.component';
 
 @Component({
   selector: 'app-campaigns-details',
   standalone: true,
-  imports: [CommonModule, NotificationComponent,RouterLink],
+  imports: [CommonModule, NotificationComponent, RouterLink],
   templateUrl: './campaigns-details.component.html',
-  styleUrl: './campaigns-details.component.scss'
+  styleUrl: './campaigns-details.component.scss',
 })
-export class CampaignsDetailsComponent implements OnInit{
+export class CampaignsDetailsComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
   private readonly apollo = inject(Apollo);
   private readonly destroyRef = inject(DestroyRef);
-private readonly campaignService = inject(CampaignService)
-   campaignId!: number;
-campaign: any;
+  private readonly campaignService = inject(CampaignService);
+  campaignId!: number;
+  campaign: any;
   donors: any[] = [];
   loading = true;
-error = false;
+  error = false;
 
-/**
- * Angular lifecycle hook that is called after data-bound properties of a component are initialized.
- * Fetches the campaign details and list of donors for the given campaign ID from the route.
- * Handles loading and error state management.
- */
-ngOnInit(): void {
-  this.campaignId = +this.route.snapshot.paramMap.get('id')!;
-  this.loading = true;
+  /**
+   * Angular lifecycle hook that is called after data-bound properties of a component are initialized.
+   * Fetches the campaign details and list of donors for the given campaign ID from the route.
+   * Handles loading and error state management.
+   */
+  ngOnInit(): void {
+    this.campaignId = +this.route.snapshot.paramMap.get('id')!;
+    this.loading = true;
 
-  this.campaignService.getCampaignDetails(this.campaignId)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe({
-      next: (res: any) => {
-        this.campaign = res.data?.campaign;
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = true;
-        this.loading = false;
-      }
-    });
+    this.campaignService
+      .getCampaignDetails(this.campaignId)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (res: any) => {
+          this.campaign = res.data?.campaign;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.error = true;
+          this.loading = false;
+        },
+      });
 
-  this.campaignService.getDonors(this.campaignId)
-    .pipe(takeUntilDestroyed(this.destroyRef))
-    .subscribe({
-      next: (donors: any[]) => this.donors = donors,
-      error: (err) => {
-        this.donors = [];
-      }
-    });
+    this.campaignService
+      .getDonors(this.campaignId)
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe({
+        next: (donors: any[]) => (this.donors = donors),
+        error: (err) => {
+          this.donors = [];
+        },
+      });
+  }
 }
-}
-
-
